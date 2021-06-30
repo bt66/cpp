75d677d45bf29ebee41930f54037f6b19ba3824a
root <crkndevops@gmail.com>
2021-06-30 20:38:49 +0700

    final project update

--- final_Project/gudang.cpp
+++ final_Project/gudang.cpp
@@ -11,16 +11,14 @@ struct barang
     int awal, akhir;
 } antrean;
 
-int main()
+void init()
 {
-    int jumlahBarang;
-    string temp;
-    cout << "Aplikasi Gudang" << endl;
     antrean.awal = 0;
     antrean.akhir = 0;
-    cout << "masukkan jumlah barang : ";
-    cin >> jumlahBarang;
-    // input data
+}
+void inputData(int jumlahBarang)
+{
+    string temp;
     for (int i = 0; i < jumlahBarang; i++)
     {
         if (antrean.akhir < jumlahBarang + antrean.akhir)
@@ -34,141 +32,192 @@ int main()
             antrean.akhir++;
         }
     }
-
-    cout << "masukkan berapa data lagi : ";
-    cin >> jumlahBarang;
-    for (int i = 0; i < jumlahBarang; i++)
-    {
-        if (antrean.akhir < jumlahBarang + antrean.akhir)
-        {
-            cout << "masukkan barang ke " << antrean.akhir << endl;
-            cin >> temp;
-            cout << "data masuk" << endl;
-            antrean.namaBarang[antrean.akhir] = temp;
-            antrean.id[antrean.akhir] = antrean.akhir;
-
-            antrean.akhir++;
-        }
-    }
-    // cetak data
-    cout << "antrean.akhir" << antrean.akhir << endl;
-
-    for (int i = antrean.awal; i < antrean.akhir; i++)
-    {
-        cout << "barang dengan id ke" << antrean.id[i] << " = " << antrean.namaBarang[i] << endl;
-    }
-
-    int keluar;
-    // keluar gudang
-    cout << "keluar berapa barang? ";
-    cin >> keluar;
-
-    for (int i = 0; i < antrean.akhir; i++)
-    {
-        antrean.id[i] = antrean.id[i + 1];
-        antrean.namaBarang[i] = antrean.namaBarang[i + 1];
-    }
-
-    cout << "antrean.akhir" << antrean.akhir << endl;
-
+}
+void periksaBarang()
+{
     for (int i = antrean.awal; i < antrean.akhir; i++)
     {
         cout << "barang dengan id ke" << antrean.id[i] << " = " << antrean.namaBarang[i] << endl;
     }
 }
 
-// menginisialisasi queue
-// void inisialisasi()
-// {
-//     antrean.awal = -1;
-//     antrean.akhir = -1;
-// }
+void barangKeluar()
+{
+    for (int i = 0; i < antrean.akhir; i++)
+    {
+        antrean.id[i] = antrean.id[i + 1];
+        antrean.namaBarang[i] = antrean.namaBarang[i + 1];
+    }
+}
 
-// bool penuh()
-// {
-//     if (antrean.akhir == KAPASITAS - 1)
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         return false;
-//     }
-// }
+// sorting berdasarkan nama barang menggunakan bubble sort
+void sortNama()
+{
+    string temp1;
+    int temp2;
+    for (int i = antrean.awal; i < antrean.akhir; i++)
+    {
+        for (int j = i + 1; j < antrean.akhir; j++)
+        {
+            if (antrean.namaBarang[j] < antrean.namaBarang[i])
+            {
+                // untuk namaBarang
+                temp1 = antrean.namaBarang[i];
+                antrean.namaBarang[i] = antrean.namaBarang[j];
+                antrean.namaBarang[j] = temp1;
 
-// bool kosong()
-// {
-//     if (antrean.akhir == -1)
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         return false;
-//     }
-// }
+                // untuk id Barang
+                temp2 = antrean.id[i];
+                antrean.id[i] = antrean.id[j];
+                antrean.id[j] = temp2;
+            }
+        }
+    }
+}
 
-// // input data
-// void inputBarang(int jumlahBarang)
-// {
-//     string temp;
-//     for (int i = antrean.akhir; i < (antrean.akhir + jumlahBarang); i++)
-//     {
-//         cout << "silahkan masukkan nama barang : ";
-//         cin >> temp;
-//         cout << "data masuk " << endl;
-//         antrean.namaBarang[antrean.akhir] = temp;
-//         antrean.id[antrean.akhir] = antrean.akhir;
-//     }
+void sortID()
+{
+    string temp1;
+    int temp2;
+    for (int i = antrean.awal; i < antrean.akhir; i++)
+    {
+        for (int j = i + 1; j < antrean.akhir; j++)
+        {
+            if (antrean.id[j] < antrean.id[i])
+            {
+                // untuk namaBarang
+                temp1 = antrean.namaBarang[i];
+                antrean.namaBarang[i] = antrean.namaBarang[j];
+                antrean.namaBarang[j] = temp1;
 
-//     // if (!penuh)
-//     // {
-//     //     antrean.akhir++;
-//     //     cout << "Silahkan masukkan nama barang : ";
-//     //     cin >> temp;
-//     //     cout << "Data masuk" << endl;
-//     //     antrean.namaBarang[antrean.akhir] = temp;
-//     //     antrean.id[antrean.akhir] = antrean.akhir;
-//     // }
-//     // else
-//     // {
-//     //     cout << "Gudang sudah penuh" << endl;
-//     // }
-//     // getchar();
-// }
+                // untuk id Barang
+                temp2 = antrean.id[i];
+                antrean.id[i] = antrean.id[j];
+                antrean.id[j] = temp2;
+            }
+        }
+    }
+}
+void cariBarang(string cari)
+{
+    int searchNama[KAPASITAS];
+    int j;
+    j = 0;
+    for (int i = antrean.awal; i < antrean.akhir; i++)
+    {
+        if (antrean.namaBarang[i] == cari)
+        {
+            searchNama[j] = 1;
+            j++;
+        }
+    }
+    if (j > 0)
+    {
+        cout << "data yang di cari adalah : " << cari << endl;
+        cout << "data tersebut terdapat pada index array ke : ";
+        for (int i = 0; i < j; i++)
+        {
+            cout << searchNama[i] << endl;
+        }
+    }
+    else
+    {
+        cout << "data tidak di temukan " << endl;
+    }
+}
 
-// // print barang
-// void periksaBarang()
-// {
-//     for (int i = antrean.awal; i < antrean.akhir; i++)
-//     {
-//         cout << "id barang : ";
-//         cout << antrean.id[i] << endl;
-//         cout << "Nama Barang :";
-//         cout << antrean.namaBarang[i] << " " << endl;
-//     }
-// }
+void hapusSemuaData()
+{
+    antrean.akhir = 0;
+}
 
-// void keluarGudang()
-// {
-//     int i;
+int main()
+{
+    int jumlahBarang;
+    string temp1;
+    int temp2;
+    int menu;
+    string cariNamaBarang;
 
-//     if (!kosong())
-//     {
-//         for (int i = 0; i < antrean.akhir; i++)
-//         {
-//             antrean.id[i] = antrean.id[i + 1];
-//         }
-//     }
-//     else
-//     {
-//         cout << "Gudang masih kosong";
-//     }
-//     getchar();
-// }
+    init();
+    /*
+    cout << "===============Aplikasi Gudang=============" << endl;
 
-// void bersihkanGudangDariBarang()
-// {
-//     antrean.awal = -1;
-//     antrean.akhir = -1;
-// }
+    // input data
+    inputData(jumlahBarang);
+    periksaBarang();
+    cout << "antrean.akhir" << antrean.akhir << endl;
+    //hapus data
+    // print data
+    periksaBarang();
+
+    // bubble sort
+    sortNama();
+
+    periksaBarang();
+
+    sortID();
+    periksaBarang();
+
+    // search by name
+
+    cariBarang(cariNamaBarang);
+
+    // reset
+
+    periksaBarang();
+    inputData(jumlahBarang);
+    periksaBarang();
+    */
+    do
+    {
+        cout << "===============Aplikasi Gudang=============" << endl;
+        cout << "Silahkan pilih : " << endl;
+        cout << "\t 1. Input Data " << endl;
+        cout << "\t 2. Lihat data" << endl;
+        cout << "\t 3. Keluarkan dari gudang" << endl;
+        cout << "\t 4. Urutkan Data berdasarkan ID" << endl;
+        cout << "\t 5. Urutkan Data berdasarkan Nama Barang" << endl;
+        cout << "\t 6. Cari Data" << endl;
+        cout << "\t 7. Bersihkan Gudang dari barang" << endl;
+        cout << "\t 8. keluar \n"
+             << endl;
+        cout << "Masukkan pilihan :";
+        cin >> menu;
+
+        switch (menu)
+        {
+        case 1:
+            cout << "masukkan jumlah barang : ";
+            cin >> jumlahBarang;
+            inputData(jumlahBarang);
+            break;
+        case 2:
+            periksaBarang();
+            break;
+        case 3:
+            barangKeluar();
+            break;
+        case 4:
+            sortID();
+            break;
+        case 5:
+            sortNama();
+            break;
+        case 6:
+            cout << "masukkan nama barang yang ingin di cari : ";
+            cin >> cariNamaBarang;
+            cariBarang(cariNamaBarang);
+            break;
+        case 7:
+            cout << "menghapus data ..." << endl;
+            hapusSemuaData();
+            cout << "data sudah di hapus" << endl;
+            break;
+
+        default:
+            cout << "masukkan angka sesuai di menu" << endl;
+            break;
+        }
+    } while (menu != 8);
+}
